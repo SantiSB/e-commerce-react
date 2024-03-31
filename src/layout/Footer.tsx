d1f15fe6de4e590dc802fd66a050adb9b706b2cd
Santiago <santi@linkedai.co>
2024-03-30 21:38:29 -0500

    Footer Styled Components

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -8,7 +8,7 @@ import {
   IconButton,
 } from '@mui/material'
 import { Link } from 'react-router-dom'
-import { useTheme } from '@mui/material/styles'
+import { styled, useTheme } from '@mui/material/styles'
 import MainLogo from '../assets/MainLogo'
 import InstagramIcon from '../assets/icons/social/InstagramIcon'
 import LinkedInIcon from '../assets/icons/social/LinkedInIcon'
@@ -17,27 +17,57 @@ import MailIcon from '../assets/icons/MailIcon'
 import PhoneIcon from '../assets/icons/PhoneIcon'
 import LocationIcon from '../assets/icons/LocationIcon'
 
+interface OrderProps {
+  order: {
+    xs?: number
+    sm?: number
+    md?: number
+    lg?: number
+    xl?: number
+  }
+}
+
+// Styled components
+const FooterBox = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  color: theme.palette.primary.main,
+  padding: theme.spacing(6, 0),
+}))
+
+const SocialMediaBox = styled(Box)({
+  display: 'flex',
+  justifyContent: 'flex-end',
+})
+
+const ContactInfoBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'flex-end',
+  gap: '8px',
+})
+
+// Styled Grid component to apply order for responsive design
+const OrderedGrid = styled(Grid)<OrderProps>(({ theme, order }) => ({
+  [theme.breakpoints.up('xs')]: {
+    order: order?.xs ?? 0,
+  },
+  [theme.breakpoints.up('md')]: {
+    order: order?.md ?? 0,
+  },
+}))
+
 const Footer: React.FC = () => {
   const theme = useTheme()
-
   return (
-    <Box
-      component="footer"
-      sx={{
-        bgcolor: theme.palette.background.paper,
-        color: theme.palette.primary.main,
-        py: 6,
-      }}
-    >
+    <FooterBox component="footer">
       <Container maxWidth="lg">
         <Grid container alignItems="flex-start" spacing={5}>
-          <Grid
+          <OrderedGrid
             item
             xs={6}
             sm={6}
             md={3}
             lg={3}
-            sx={{ order: { xs: 1, md: 1 } }}
+            order={{ xs: 1, md: 1 }}
           >
             <Link
               to="/"
@@ -50,23 +80,19 @@ const Footer: React.FC = () => {
             >
               <MainLogo height={40} />
             </Link>
-          </Grid>
+          </OrderedGrid>
 
-          <Grid
+          <OrderedGrid
             item
             xs={6}
             sm={6}
             md={3}
             lg={3}
-            sx={{
-              order: { xs: 2, md: 4 },
-              display: 'flex',
-              justifyContent: 'flex-end',
-            }}
+            order={{ xs: 2, md: 4 }}
           >
-            <Box>
+            <SocialMediaBox>
               <IconButton>
-                <FacebookIcon color={theme.palette.primary.main}/>
+                <FacebookIcon color={theme.palette.primary.main} />
               </IconButton>
               <IconButton>
                 <InstagramIcon color={theme.palette.primary.main}/>
@@ -74,16 +100,16 @@ const Footer: React.FC = () => {
               <IconButton>
                 <LinkedInIcon color={theme.palette.primary.main}/>
               </IconButton>
-            </Box>
-          </Grid>
+            </SocialMediaBox>
+          </OrderedGrid>
 
-          <Grid
+          <OrderedGrid
             item
             xs={12}
             sm={6}
             md={3}
             lg={3}
-            sx={{ order: { xs: 3, sm: 3, md: 2 } }}
+            order={{ xs: 3, sm: 3, md: 2 }}
           >
             <Typography variant="h6" gutterBottom>
               Información
@@ -94,60 +120,42 @@ const Footer: React.FC = () => {
             <Typography variant="body1" gutterBottom>
               Preguntas Frecuentes
             </Typography>
-          </Grid>
+          </OrderedGrid>
 
-          <Grid
+          <OrderedGrid
             item
             xs={12}
             sm={6}
             md={3}
             lg={3}
-            sx={{ order: { xs: 4, sm: 2, md: 3 } }}
+            order={{ xs: 4, sm: 2, md: 3 }}
           >
             <Typography variant="h6" gutterBottom>
               Contacto
             </Typography>
 
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'flex-end',
-                gap: 1,
-              }}
-            >
+            <ContactInfoBox>
               <MailIcon />
               <Typography variant="body2">ecommercs@gmail.com</Typography>
-            </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'flex-end',
-                gap: 1,
-              }}
-            >
+            </ContactInfoBox>
+            <ContactInfoBox>
               <PhoneIcon />
               <Typography variant="body2">+57 3542564875</Typography>
-            </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'flex-end',
-                gap: 1,
-              }}
-            >
+            </ContactInfoBox>
+            <ContactInfoBox>
               <LocationIcon />
               <Typography variant="body2">
                 Carrera 34 #53-45 Pasto, Colombia
               </Typography>
-            </Box>
-          </Grid>
+            </ContactInfoBox>
+          </OrderedGrid>
         </Grid>
         <Divider sx={{ my: 6 }} />
         <Typography variant="body2" align="center">
           © 2023 Flowbite™. All Rights Reserved.
         </Typography>
       </Container>
-    </Box>
+    </FooterBox>
   )
 }
 
